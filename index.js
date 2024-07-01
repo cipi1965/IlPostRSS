1e40cc3bf73fb68a08bfd78e98c94b6faf6b4b17
Matteo Piccina <matteo.piccina@cvm.it>
2024-07-01 19:54:49 +0200

    Add descriptions of episodes

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import {fetch, CookieJar} from "node-fetch-cookies";
 import Fastify from "fastify";
 import { Podcast } from "podcast";
 import 'dotenv/config'
+import path from 'path'
 import podcasts from './podcasts.js'
+import { parse } from 'node-html-parser';
+import fs from 'fs';
+import { stripHtml } from "string-strip-html";
 
 const fastify = Fastify({ logger: true })
+fastify.register(import('@fastify/static'), {
+    root: path.join(import.meta.dirname, 'assets'),
+    prefix: '/assets/',
+})
 
 const cookieJar = new CookieJar()
 
@@ -25,27 +33,48 @@ const login = await fetch(cookieJar,"https://www.ilpost.it/wp-login.php", {
 
 for (const [key, value] of Object.entries(podcasts)) {
     fastify.get(`/${key}`, async (request, reply) => {
-        const podcast = await fetch(cookieJar, "https://www.ilpost.it/wp-admin/admin-ajax.php", {
+        const podcastInfo = await fetch(cookieJar, `https://www.ilpost.it/episodes/podcasts/${value.stringId}/`)
+        const podcastInfoParsed = parse(await podcastInfo.text())
+        const description = podcastInfoParsed.querySelector('.ilpostPodcastDesc').text;
+
+        const podcastList = await fetch(cookieJar, "https://www.ilpost.it/wp-admin/admin-ajax.php", {
             body: `action=checkpodcast&post_id=0&podcast_id=${value.id}`,
             method: "POST",
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
             }
         })
-        const podcastJson = await podcast.json()
+        const podcastListJson = await podcastList.json()
 
         const feed = new Podcast({
             title: 'Indagini',
             imageUrl: value.cover,
-            feedUrl: 'http://192.168.192.168:3000/',
+            description: description,
+            feedUrl: `${process.env.BASE_URL}/${key}`,
             author: 'Il Post'
         })
 
-        for (const podcastEpisode of podcastJson['data']['postcastList']) {
+        for (const podcastEpisode of podcastListJson['data']['postcastList']) {
+            const cachePath = path.join(import.meta.dirname, 'cache', `${podcastEpisode.id}.json`)
+            let episodeInfo = null
+
+            if (fs.existsSync(cachePath)) {
+                episodeInfo = JSON.parse(fs.readFileSync(cachePath, 'utf8'));
+            } else {
+                const episodeInfoResponse = await fetch(cookieJar, `https://www.ilpost.it/wp-json/wp/v2/episodes/${podcastEpisode.id}`)
+
+                const episodeInfoText = await episodeInfoResponse.text()
+                fs.writeFileSync(cachePath, await episodeInfoText);
+                episodeInfo = JSON.parse(episodeInfoText)
+            }
+
+            const episodeDescription = stripHtml(episodeInfo.content.rendered).result
+
             feed.addItem({
                 title: podcastEpisode.title,
                 url: podcastEpisode.url,
                 date: podcastEpisode.date,
+                description: episodeDescription,
                 enclosure: {
                     url: podcastEpisode.podcast_raw_url,
                     type: 'audio/mpeg'
--- podcasts.js
+++ podcasts.js
@@ -2,5 +2,6 @@ export default {
     'morning': {
         id: 227474,
         cover: 'https://www.ilpost.it/wp-content/uploads/2021/05/evening-1.png',
+        stringId: 'morning'
     }
 }
