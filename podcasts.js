f7a6c7171e94df2c9fb42968960ee7568dee0caf
Matteo Piccina <matteo.piccina@cvm.it>
2024-07-01 20:21:38 +0200

    Add subscriber podcasts

--- podcasts.js
+++ podcasts.js
@@ -4,5 +4,29 @@ export default {
         id: 227474,
         cover: 'https://www.ilpost.it/wp-content/uploads/2021/05/evening-1.png',
         stringId: 'morning'
+    },
+    'altre-indagini': {
+        title: 'Altre Indagini',
+        id: 236670,
+        cover: 'https://www.ilpost.it/wp-content/uploads/2024/04/09/1712669037-altre-Indagini-logo-1.png',
+        stringId: 'altre-indagini'
+    },
+    'tienimi-bordone': {
+        title: 'Tienimi Bordone',
+        id: 227193,
+        cover: 'https://www.ilpost.it/wp-content/uploads/2021/04/app-tb.jpg',
+        stringId: 'tienimi-bordone'
+    },
+    'podcast-eurovision': {
+        title: "Un podcast sull'Eurovision",
+        id: 227496,
+        cover: 'https://www.ilpost.it/wp-content/uploads/2024/04/30/1714468608-copertina500x500.jpg',
+        stringId: 'podcast-eurovision'
+    },
+    'il-podcast-del-post-su-sanremo': {
+        title: 'Un podcast su Sanremo',
+        id: 227196,
+        cover: 'https://www.ilpost.it/wp-content/uploads/2024/02/01/1706806154-copertina500x500.jpg',
+        stringId: 'il-podcast-del-post-su-sanremo'
     }
 }
